9b7740cb76f30a2a7fed46e138f72c7e4a2aaa5b
pErfEcto2 <misha.yaro2004@gmail.com>
2025-04-15 13:34:21 +0300

    waste collection points table renamed

--- README.md
+++ README.md
@@ -5,8 +5,9 @@
 2) `pip3 -r requirements.txt`
 3) `chmod 755 run.sh`
 4) `chmod 755 setup.sh`
-5) add to the table "waste_collection_points" in the users.db 
-5) `./setup.sh`
+5) add to the table "buildings" in the users.db neccessary places
+6) `./setup.sh`
+7) add facts to the `src/data/facts.txt` (separated by newline)
 
 ## Run
 1) Put in the `src/config.py` API_KEY, PLACES_FILE_PATH, FACTS_FILE_PATH, DB_NAME, SECRET_PASSWORD and MONITOR_CHAT_ID 
--- src/db_init.py
+++ src/db_init.py
@@ -2,6 +2,6 @@ import lib
 
 
 lib.exec_query("create table if not exists users (id bigint primary key, building_id bigint);")
-lib.exec_query("create table if not exists waste_collection_points (id bigint primary key, address text);")
+lib.exec_query("create table if not exists buildings (id bigint primary key, address text);")
 lib.exec_query("create table if not exists feedbacks (timestamp datetime default current_timestamp primary key, feedback text);")
 
