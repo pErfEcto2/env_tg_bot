9442342a13e19fa5972923cbef594d2013657401
pErfEcto2 <misha.yaro2004@gmail.com>
2025-04-22 22:12:15 +0300

    counting every usage

--- src/db_init.py
+++ src/db_init.py
@@ -1,7 +1,7 @@
 import lib
 
 
-lib.exec_query("create table if not exists users (id bigint primary key, building_id bigint);")
+lib.exec_query("create table if not exists users (id bigint primary key, building_id bigint, count bigint);")
 lib.exec_query("create table if not exists buildings (id bigint primary key, address text);")
 lib.exec_query("create table if not exists feedbacks (timestamp datetime default current_timestamp primary key, feedback text);")
 lib.exec_query("create table if not exists plastic (id bigint primary key, building_id bigint, description text, address text, lat real, lon real);")
